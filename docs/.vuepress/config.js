5fc3842097c4acae542f967e5e70ad7c0243b896
5201314999 <1446418561@qq.com>
2018-10-29 17:31:29 +0800

    更新readme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     nav: [
       { text: '首页', link: '/' },
       { text: '关于', link: '/about/' },
-      { text: 'github', link: 'https://github.com/5201314999' }
+      { text: 'github', link: 'https://github.com/5201314999/jrNote' }
     ]
   }
 };
