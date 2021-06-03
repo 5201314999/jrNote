fd83cc09e248135d8c66a1388d2ceef7b855d2e2
jr <jr@jrdeMacBook-Pro.local>
2021-06-03 23:46:23 +0800

    feat: demo

---
+++ docs/work/midea/demo2.tsx
@@ -0,0 +1,31 @@
+interface Meeting {
+    creatorId: string;
+    starTime: number;
+    endTime: number;
+    permissions ?: {
+        canCancel: boolean;
+        canEdit: boolean;
+        canInvite: boolean;
+    }
+}
+
+interface Props {
+    meeting: Meeting;
+    userId: string;
+}
+
+const ReactComponent = ({ meeting, userId }: Props) => {
+    const meetingHasStarted = new Date() > new Date(meeting.startTime)
+    const hasCreatedMeeting = userId === meeting.creatorId
+    const canCancel = hasCreatedMeeting || (!!meeting.permissions && meeting.permissions.canCancel)
+    return (
+        <div>
+            <h2>标题</h2>
+            <div>内容</div>
+            {
+                meetingHasStarted && canCancel && (<button> 取消 </button>)
+            }
+        </div>
+    )
+}
+
